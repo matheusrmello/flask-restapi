e4c1b4bab81f22b929c6ed91d47f284eb24c9202
matheusmello09 <matheus.rm.09@hotmail.com>
2025-01-07 18:29:07 -0300

    feat: Add Docker-compose to build dev env

--- app.py
+++ app.py
@@ -4,19 +4,22 @@ from flask_restful import Api, Resource
 app = Flask(__name__)
 api = Api(app)
 
+
 class Users(Resource):
     def get(self):
         return {"message": "user 1"}
-    
+
+
 class User(Resource):
     def get(post):
         return {"message": "test"}
-    
+
     def get(self, cpf):
         return {"message": "CPF"}
+
 
 api.add_resource(Users, '/users')
 api.add_resource(User, '/user', '/user/<string:cpf>')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0", port=5000)
