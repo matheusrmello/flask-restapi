44f994237589f63a7ac966540eaebe6466d3480f
matheusrmello <matheus.rm.09@hotmail.com>
2025-01-18 13:57:53 -0300

    refactor: using application factory

--- application/app.py
+++ application/app.py
@@ -1,22 +1,11 @@
-from flask import Flask
-from flask_restful import Api, Resource, reqparse
-from flask_mongoengine import MongoEngine
+from flask import Flask, jsonify
+from flask_restful import Resource, reqparse
 from mongoengine import NotUniqueError
+from .model import UserModel
 import re
 
-app = Flask(__name__)
 
-app.config["MONGODB_SETTINGS"] = [
-    {
-        "db": "users",
-        "host": "mongodb",
-        "port": 27017,
-        "username": "root",
-        "password": "123456admin"
-    }
-]
-
-_user_parser=reqparse.RequestParser()
+_user_parser = reqparse.RequestParser()
 _user_parser.add_argument('first_name',
                           type=str,
                           required=True,
@@ -43,23 +32,16 @@ _user_parser.add_argument('birth_date',
                           help='This field cannot be blank!'
                           )
 
-api = Api(app)
-db = MongoEngine(app)
 
-class UserModel(db.Document):
-    cpf = db.StringField(required=True, unique=True)
-    first_name = db.StringField(required=True)
-    last_name = db.StringField(required=True)
-    email = db.EmailField(required=True)
-    birth_date = db.DateTimeField(required=True)
 
 class Users(Resource):
-    
+
     def get(self):
-        return {"message": "user 1"}
+        return jsonify(UserModel.objects())
+
 
 class User(Resource):
-    
+
     def validate_cpf(self, cpf):
         if not re.match(r'\d{3}\.\d{3}\.\d{3}-\d{2}', cpf):
             return False
@@ -69,20 +51,22 @@ class User(Resource):
         if len(numbers) != 11 or len(set(numbers)) == 1:
             return False
 
-        sum_of_products = sum(a*b for a, b in zip(numbers[0:9], range(10, 1, -1)))
+        sum_of_products = sum(a*b for a, b in zip(numbers[0:9],
+                                                  range(10, 1, -1)))
         expected_digit = (sum_of_products * 10 % 11) % 10
         if numbers[9] != expected_digit:
             return False
 
-        sum_of_products = sum(a*b for a, b in zip(numbers[0:10], range(11, 1, -1)))
+        sum_of_products = sum(a*b for a, b in zip(numbers[0:10],
+                                                  range(11, 1, -1)))
         expected_digit = (sum_of_products * 10 % 11) % 10
         if numbers[10] != expected_digit:
             return False
 
         return True
-    
+
     def post(self):
-        data= _user_parser.parse_args()
+        data = _user_parser.parse_args()
         if not self.validate_cpf(data["cpf"]):
             return {"message": "CPF is invalid!"}, 400
         try:
@@ -90,13 +74,9 @@ class User(Resource):
             return {"message": "User %s successfully created!" % response.id}
         except NotUniqueError:
             return {"message": "CPF already exist in database!"}, 400
-    
-    def get(self):
-        return {"message": "CPF"}
 
-
-api.add_resource(Users, '/users')
-api.add_resource(User, '/user', '/user/<string:cpf>')
-
-if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    def get(self, cpf, Response):
+        response = UserModel.objects(cpf=cpf)
+        if response:
+            return jsonify(response)
+        return {"message": "User does not exist in database!"}, 400
---
+++ config.py
@@ -0,0 +1,9 @@
+class DevConfig():
+    MONGODB_SETTINGS = [
+        {
+            "db": "users",
+            "host": "mongodb",
+            "username": "root",
+            "password": "123456admin"
+        }
+    ]
